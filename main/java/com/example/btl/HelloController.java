b06a8fa1634f8b2d7e6a6c75ee65a2ab2fa886c0
kieeen <99831816+kieeen@users.noreply.github.com>
2022-03-04 21:25:18 +0700

    Add files via upload

---
+++ main/java/com/example/btl/HelloController.java
@@ -0,0 +1,68 @@
+package com.example.btl;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import  java.nio.file.Files;
+import java.io.File;
+
+public class HelloController {
+    @FXML
+    private ListView<File> listView;
+    @FXML
+    private TreeView<File> treeView;
+    @FXML
+    private TextField input;
+    @FXML
+    private Button search;
+
+    public void InitTreeView(){
+        File tmp = new File("/");
+        TreeItem<File> root = new TreeItem<File>(tmp);
+        File[] files = File.listRoots();
+        for(File file:files){
+           TreeItem<File> node = new TreeItem<File>(file);
+           root.getChildren().add(node);
+        }
+        treeView.setRoot(root);
+        treeView.setShowRoot(false);
+    }
+
+    public void Select(){
+        try{
+            TreeItem<File> item = treeView.getSelectionModel().getSelectedItem();
+            File tmp = item.getValue();
+            if(item.isLeaf()){
+                if(!tmp.isFile()){
+                    File[] files = tmp.listFiles();
+                    for(File file:files){
+                        TreeItem<File> node = new TreeItem<File>(file);
+                        item.getChildren().add(node);
+                    }
+                }
+            }
+            else{
+                if(item.isExpanded()){
+                    item.setExpanded(false);
+                }
+                else{
+                    item.setExpanded(true);
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex);
+        }
+
+    }
+
+
+    public void search() {
+        File text = input.getText();
+        
+        listView.getItems().add(text);
+    }
+}
